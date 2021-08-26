98dea0429a9113f78ec5075eefae4c333b49dc03
Stalin Beltran <stalin.beltran2006@gmail.com>
2021-08-26 14:36:12 -0500

    corregido url para que sea relativo a la raiz

--- resources/views/Products/Index.blade.php
+++ resources/views/Products/Index.blade.php
@@ -38,11 +38,11 @@
                 <td>
                     <form action="" method="POST">
 
-                        <a href="{{$product->id}}" title="show">
+                        <a href="/products/{{$product->id}}" title="show">
                             <i class="fas fa-eye text-success  fa-lg"></i>
                         </a>
 
-                        <a href="{{$product->id}}/edit">
+                        <a href="/products/{{$product->id}}/edit">
                             <i class="fas fa-edit  fa-lg"></i>
                         </a>
 
