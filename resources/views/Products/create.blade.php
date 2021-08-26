e90d89592d6b616a8451460630403506aabc4646
Stalin Beltran <stalin.beltran2006@gmail.com>
2021-08-26 12:55:00 -0500

    corregido nombre de textarea (no guardaba)

--- resources/views/Products/create.blade.php
+++ resources/views/Products/create.blade.php
@@ -35,7 +35,7 @@
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>Description:</strong>
-                    <textarea class="form-control" style="height:50px" name="introduction"
+                    <textarea class="form-control" style="height:50px" name="description"
                         placeholder="description"></textarea>
                 </div>
             </div>
